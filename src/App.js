e909457cc73c94a49afba575d20d38465e2a9ac5
esatyasar <esatyasar91@gmail.com>
2021-10-08 14:17:36 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <PortfolioProvider>
       <AppRouter/>
-      <div>Hello</div>
     </PortfolioProvider>
         );
 }
--- src/app-router/AppRouter.js
+++ src/app-router/AppRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {  BrowserRouter as Router,  Switch,  Route} from "react-router-dom";
-import Navbar from "../components/Navbar"
+import {Navbar} from "../components/Navbar"
 import Footer from "../components/Footer"
 import Home from "../pages/Home"
 import DetailsCard from "../pages/DetailsCard"
