8fb36c27d820d82dbf84fce89cfa3b9f4d369c72
esatyasar <esatyasar91@gmail.com>
2021-10-18 17:02:05 +0300

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
+import { CopyAllSharp } from '@mui/icons-material'
 import React from 'react'
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  footer :{
+    textAlign : "center"
+  }
+}));
 
 function Footer() {
+    const classes = useStyles();
+    const year = new Date()
     return (
-        <div>
-            Footer
+        <div className = {classes.footer}>
+            <p> Copyright &copy; {year.getFullYear()} Esat Yasar</p>
         </div>
     )
 }
